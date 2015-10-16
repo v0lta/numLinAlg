410943b3923ebc96487f7f429579ded1aa49f92b
v0lta <moritzwolter@gmx.de>
2015-10-16 12:43:59 +0200

    model order reduction.

--- hw2/computeTf.m
+++ hw2/computeTf.m
@@ -21,19 +21,52 @@ h2Norm = sqrt(1/(pi) * abs(H'*H));
 
 
 %% modal truncation
-[V1,D1] = eigs(A,E,1);
-[V5,D5] = eigs(A,E,5);
-[V10,D10] = eigs(A,E,10);
-[V50,D50] = eigs(A,E,50);
+[V1,D1] = eigs(A,E,1,'lr');
+[V5,D5] = eigs(A,E,5,'lr');
+[V10,D10] = eigs(A,E,10,'lr');
+[V50,D50] = eigs(A,E,50,'lr');
 
-approxA1 = V1*D1*V1';
-approxA5 = V5*D5*V5';
-approxA10 = V10*D10*V10';
-approxA50 = V50*D50*V50';
+V1 = orth(V1);
+V5 = orth(V5);
+V10 = orth(V10);
+V50 = orth(V50);
+
+apxA1 = V1'*A*V1;
+apxB1 = V1'*B;
+apxE1 = V1'*E*V1;
+
+apxA5 = V5'*A*V5;
+apxB5 = V5'*B;
+apxE5 = V5'*E*V5;
+
+apxA10 = V10'*A*V10;
+apxB10 = V10'*B;
+apxE10 = V10'*E*V10;
+
+apxA50 = V50'*A*V50;
+apxB50 = V50'*B;
+apxE50 = V50'*E*V50;
 
 for k =1:length(s)
-    H1(k) = B'*((s(k)*E(1,1) - approxA1)\B);
-    H5(k) = B'*((s(k)*E - approxA5)\B);
-    H10(k) = B'*((s(k)*E - approxA10)\B);
-    H50(k) = B'*((s(k)*E - approxA50)\B);
+    H1(k) = apxB1'*((s(k)*apxE1 - apxA1)\apxB1);
+    H5(k) = apxB5'*((s(k)*apxE5 - apxA5)\apxB5);
+    H10(k) = apxB10'*((s(k)*apxE10 - apxA10)\apxB10);
+    H50(k) = apxB50'*((s(k)*apxE50 - apxA50)\apxB50);
 end
+
+figure
+loglog(abs(H))
+hold on
+loglog(abs(H1))
+loglog(abs(H5))
+loglog(abs(H10))
+loglog(abs(H50))
+legend('original','h1','h5','h10','h50')
+
+%% Krylov subspace
+
+
+
+
+
+
