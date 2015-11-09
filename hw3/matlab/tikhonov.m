66829c739ae51db62cca9162854e37bcf06270ed
v0lta <moritzwolter@gmx.de>
2015-11-09 16:41:25 +0100

    removed for loop.

--- hw3/matlab/tikhonov.m
+++ hw3/matlab/tikhonov.m
@@ -39,11 +39,13 @@ function [ xReg ] = tikhonov( A,b,ignorePct,plotting )
  
  
  for k = 1:n
-     x = zeros(n,1);
-     for i = 1:n
-        f = S(i,i)^2 / (S(i,i)^2 + lambda(k)^2); 
-        x = x + f*((U(:,i)'*b)/S(i,i)) * V(:,i);   
-     end
+%      for i = 1:n
+%         f = S(i,i)^2 / (S(i,i)^2 + lambda(k)^2); 
+%         x = x + f*((U(:,i)'*b)/S(i,i)) * V(:,i);   
+%      end
+     sigma = diag(S);
+     fVec = sigma.^2 ./ ( sigma.^2 + ones(n,1)*lambda(k).^2);
+     x =  sum(V*(diag(U'*b .* sigma.^(-1)))*diag(fVec),2);
      regSolNorm(k) = norm(L*x);
      residlNorm(k) = norm(A*x - b);
      xVec(:,k) = x;
